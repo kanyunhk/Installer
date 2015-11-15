4641fd023aa41b4dce905f8207e09e7779a07b19
Samson Close <stanmoon95@gmail.com>
2015-11-15 16:55:50 -0500

    Updated Mod Files

--- src/net/qwertysam/util/ModFiles.java
+++ src/net/qwertysam/util/ModFiles.java
@@ -9,40 +9,44 @@ public class ModFiles
 			
 			"/assets/minecraft/qwertysam/textures/buttons.png",
 			
-			"/net/playmcm/qwertysam/gui/GuiCustomMessages.class",
-			"/net/playmcm/qwertysam/gui/GuiCustomOneEditor.class",
-			"/net/playmcm/qwertysam/gui/GuiCustomThreeEditor.class",
-			"/net/playmcm/qwertysam/gui/GuiCustomTwoEditor.class",
+			"/net/playmcm/qwertysam/gui/GuiCustomEditor.class",
 			"/net/playmcm/qwertysam/gui/GuiIconButton.class",
 			"/net/playmcm/qwertysam/gui/ModGui.class",
 			
-			"/net/playmcm/qwertysam/io/SaveHandling.class",
+			"/net/playmcm/qwertysam/io/Option.class",
+			"/net/playmcm/qwertysam/io/OptionKey.class",
+			"/net/playmcm/qwertysam/io/OptionManager.class",
+			
+			"/net/playmcm/qwertysam/log/LogType.class",
+			"/net/playmcm/qwertysam/log/QLogger.class",
 			
 			"/net/playmcm/qwertysam/messages/api/IMessage.class",
+			"/net/playmcm/qwertysam/messages/api/LoadableMessage.class",
 			"/net/playmcm/qwertysam/messages/api/Message.class",
+			"/net/playmcm/qwertysam/messages/api/MessageSender$1.class",
+			"/net/playmcm/qwertysam/messages/api/MessageSender.class",
 			"/net/playmcm/qwertysam/messages/api/MessageType.class",
 			"/net/playmcm/qwertysam/messages/api/URL.class",
 			
 			"/net/playmcm/qwertysam/messages/BanAppealsPage.class",
 			"/net/playmcm/qwertysam/messages/BecomingStaff.class",
-			"/net/playmcm/qwertysam/messages/CustomOne.class",
-			"/net/playmcm/qwertysam/messages/CustomThree.class",
-			"/net/playmcm/qwertysam/messages/CustomTwo.class",
 			"/net/playmcm/qwertysam/messages/DonatePage.class",
 			"/net/playmcm/qwertysam/messages/ExplainArgue.class",
 			"/net/playmcm/qwertysam/messages/ExplainRDM.class",
 			"/net/playmcm/qwertysam/messages/ExplainTeaming.class",
+			"/net/playmcm/qwertysam/messages/ForumPage.class",
+			"/net/playmcm/qwertysam/messages/MessageManager.class",
 			"/net/playmcm/qwertysam/messages/ReportPage.class",
 			"/net/playmcm/qwertysam/messages/RulesPage.class",
 			"/net/playmcm/qwertysam/messages/VotePage.class",
 			
+			"/net/playmcm/qwertysam/resource/KeyPress.class",
+			
 			"/net/playmcm/qwertysam/util/AuthUtil.class",
-			"/net/playmcm/qwertysam/util/Messages.class",
-			"/net/playmcm/qwertysam/util/MessageSender$1.class",
-			"/net/playmcm/qwertysam/util/MessageSender.class",
+			"/net/playmcm/qwertysam/util/KeyPress.class",
+			"/net/playmcm/qwertysam/util/PathUtil.class",
 			"/net/playmcm/qwertysam/util/UrlOpener.class",
 			
 			"/net/playmcm/qwertysam/ModMain.class"
-	
 	});
 }
